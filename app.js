17996ae26650b34e8bb83470e356e6370d07d4f4
Guillaume Salicis <salicisguillaume@yahoo.fr>
2020-11-13 21:11:04 +0100

    end of poc

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 (function (window) {
   var prms = {
-    angularSpeed: 0.001,//Math.PI / 10;,
+    angularSpeed: 0, //0.01,//Math.PI / 10;,
+    frequency: 1, // ajoute un point tous le N rafraîchissements
+    penPosition: { x: 0, y: 0 },
     point: {
       color: '#ff0000',
       lineWidth: 1,
@@ -31,13 +33,13 @@
   var points = [];
   var pointing = null; // if mouse down
   var inking = false;
+  var step = 0;
+  var o = null;
 
   console.log('externalCircleRadius', prms.dimensions.squareSize * prms.dimensions.circleFactor);
   function draw() {
-
-
     var ctx = document.getElementById('canvas').getContext('2d');
-    currentAngleStep = currentAngle + prms.angularSpeed;
+    currentAngleStep = currentAngleStep + prms.angularSpeed;
     currentAngle = (currentAngle + prms.angularSpeed) % (2 * Math.PI);
     ctx.save();
     ctx.clearRect(0, 0, prms.dimensions.squareSize, prms.dimensions.squareSize);
@@ -66,20 +68,22 @@
     var initialcX = prms.dimensions.innerCircleRadius - externalCircleRadius + prms.dimensions.lineWidth;
     var initialcY = 0;
     var beta = currentAngleStep * prms.dimensions.innerCircleRadius / externalCircleRadius;
-    console.log(beta)
+    // console.log((new Date()).getTime(), points.length)
 
-    var o = rotatePoint({ x: initialcX, y: initialcY }, beta);
+    o = rotatePoint({ x: initialcX, y: initialcY }, beta);
 
     drawInnerCircle(ctx, o, currentAngle);
 
-    var aTestPoint = { x: 20, y: -prms.dimensions.innerCircleRadius + 20 };
+    prms.penPosition = { x: 20, y: -prms.dimensions.innerCircleRadius + 20 };
 
-    if (inking) addPoint(currentPointPosition(aTestPoint, o, currentAngle));
+    if (inking && (step % prms.frequency === 0)) addPoint(currentPointPosition(prms.penPosition, o, currentAngle));
 
     // // draw points
     points.forEach((point) => drawPoint(ctx, point));
 
     ctx.restore();
+    step++;
+    // setTimeout(() => window.requestAnimationFrame(draw), 10)
     window.requestAnimationFrame(draw)
   }
 
@@ -121,19 +125,43 @@
     ctx.beginPath();
     ctx.arc(point.x, point.y, prms.dimensions.innerCircleRadius, 0, Math.PI * 2, true);
     ctx.stroke();
-
   }
 
   function start() { inking = true; }
   function stop() { inking = false; }
+  function stopCircle() { prms.angularSpeed = 0; }
 
   function drawPoint(ctx, point) {
     ctx.beginPath();
     ctx.lineWidth = point.lineWidth;
     ctx.strokeStyle = point.color;
     ctx.fillStyle = point.color;
-    ctx.arc(point.x + prms.point.xOffset, point.y + prms.point.yOffset, point.radius, 0, Math.PI * 2, true);
+    ctx.moveTo(point.x + prms.point.xOffset, point.y + prms.point.yOffset);
+    ctx.lineTo(point.x + prms.point.xOffset + 1, point.y + prms.point.yOffset);
     ctx.stroke();
+  }
+
+  // function drawPointCircle(ctx, point) {
+  //   ctx.beginPath();
+  //   ctx.lineWidth = point.lineWidth;
+  //   ctx.strokeStyle = point.color;
+  //   ctx.fillStyle = point.color;
+  //   ctx.arc(point.x + prms.point.xOffset, point.y + prms.point.yOffset, point.radius, 0, Math.PI * 2, true);
+  //   ctx.stroke();
+  // }
+
+  /** positionne le point sur le cercle interne */
+  function setPen(event) {
+    var p = correctPosition({ x: event.layerX, y: event.layerY });
+    console.log('clic', event, p, o)
+    if (isInInnerCircle(p)) {
+      console.log('dans le cercle')
+    }
+  }
+  function isInInnerCircle(p) {
+    var translated = translatePoint(p, oppPoint(o));
+    console.log('translated', translated, 'of', p);
+    return (Math.pow(translated.x, 2) + Math.pow(translated.y, 2) < Math.pow(prms.dimensions.innerCircleRadius, 2));
   }
 
   function addPoint(p) {
@@ -149,12 +177,12 @@
   //   return (Math.pow(translated.x, 2) + Math.pow(translated.y, 2) < Math.pow(prms.dimensions.innerCircleRadius, 2));
   // }
 
-  // function correctPosition(p) {
-  //   return rotatePoint({
-  //     x: p.x * 1 / prms.dimensions.scaleFactor - prms.dimensions.squareSize / 2 * 1 / prms.dimensions.scaleFactor,
-  //     y: p.y * 1 / prms.dimensions.scaleFactor - prms.dimensions.squareSize / 2 * 1 / prms.dimensions.scaleFactor,
-  //   }, currentAngle);
-  // }
+  function correctPosition(p) {
+    return rotatePoint({
+      x: p.x * 1 / prms.dimensions.scaleFactor - prms.dimensions.squareSize / 2 * 1 / prms.dimensions.scaleFactor,
+      y: p.y * 1 / prms.dimensions.scaleFactor - prms.dimensions.squareSize / 2 * 1 / prms.dimensions.scaleFactor,
+    }, currentAngle);
+  }
 
   function rotatePoint(p, alpha) {
     return {
@@ -180,16 +208,17 @@
   var canvasElt = document.getElementById('canvas');
   canvasElt.setAttribute('width', prms.dimensions.squareSize);
   canvasElt.setAttribute('height', prms.dimensions.squareSize);
-  canvasElt.addEventListener('click', addPoint, false);
-  canvasElt.addEventListener('mousedown', (event) => { lastSeq++; pointing = event; }, false);
-  canvasElt.addEventListener('mousemove', (event) => { if (pointing) pointing = event }, false);
-  canvasElt.addEventListener('mouseup', (event) => pointing = null, false);
+  canvasElt.addEventListener('click', setPen, false);
+  // canvasElt.addEventListener('mousedown', (event) => { lastSeq++; pointing = event; }, false);
+  // canvasElt.addEventListener('mousemove', (event) => { if (pointing) pointing = event }, false);
+  // canvasElt.addEventListener('mouseup', (event) => pointing = null, false);
 
   // on fait une interface avec l'extérieur
   window.requestAnimationFrame(draw);
   window.pottingWheelPrms = prms;
   window.pottingWheelPrms.start = start;
   window.pottingWheelPrms.stop = stop;
+  window.pottingWheelPrms.stopCircle = stopCircle;
   window.pottingWheelPrms.clear = clear;
   window.pottingWheelPrms.undo = deleteLastSeq;
 })(window);
@@ -197,14 +226,14 @@
 (function (window) {
   if (window.pottingWheelPrms) {
 
-    document.getElementById('angular-speed').value = Math.round(window.pottingWheelPrms.angularSpeed * 1000);
+    document.getElementById('angular-speed').value = Math.round(window.pottingWheelPrms.angularSpeed * 100);
     document.getElementById('color').value = window.pottingWheelPrms.point.color;
     document.getElementById('lineWidth').value = window.pottingWheelPrms.point.lineWidth;
     document.getElementById('radius').value = window.pottingWheelPrms.point.radius;
 
     document.getElementById('angular-speed')
       .addEventListener('change', (event) => {
-        window.pottingWheelPrms.angularSpeed = event.target.value / 1000;
+        window.pottingWheelPrms.angularSpeed = event.target.value / 100;
       });
     mapPointOption('color', 'color');
     mapPointOption('radius', 'radius');
@@ -212,6 +241,7 @@
 
     mapAction('start', 'start');
     mapAction('stop', 'stop');
+    mapAction('stop-circle', 'stopCircle');
     mapAction('clear', 'clear');
     mapAction('undo', 'undo');
 
