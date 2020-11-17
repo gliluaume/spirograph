09b74a368dd3045dc4a91c2ae2813b536089c6ed
Guillaume Salicis <salicisguillaume@yahoo.fr>
2020-11-17 23:30:56 +0100

    v1

--- pack.sh
+++ pack.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # work around: github page seems unable to serve pages deps with href="/script.js". This replace href="/script.js" with href="script.js"
+rm -rf dist && \
 parcel build src/index.html && \
 sed 's/ href=\"\// href=\"/g' dist/index.html > dist/tmp.html && \
 sed 's/ src=\"\// src=\"/g' dist/tmp.html > dist/index.html
