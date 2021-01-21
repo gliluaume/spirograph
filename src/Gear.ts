e560a5d0046d906b7032c947c59245747a9a2494
Guillaume Salicis <gsalicis@d-edge.com>
2021-01-21 16:09:10 +0100

    fixup gears 2

--- src/Gear.ts
+++ src/Gear.ts
@@ -1,18 +1,51 @@
 import { IPoint, rotatePoint, translatePoint } from "./geometry"
 
+class Borders {
+    public padding: number
+    public maxX: number
+    public minX: number
+    public maxY: number
+    public minY: number
+
+    constructor(points: IPoint[], padding = 5) {
+        this.padding = padding
+        this.maxX = this.max(points, p => p.x)
+        this.minX = this.min(points, p => p.x)
+        this.maxY = this.max(points, p => p.y)
+        this.minY = this.min(points, p => p.y)
+    }
+
+    public asString(): string {
+        return `${this.minX - this.padding} ${this.minY - this.padding}` + ' ' +
+            `${this.maxX + this.padding} ${this.maxY + this.padding}`
+    }
+
+    public asStyle(): string {
+        return `height: ${Math.round(this.maxY + this.padding)}px; width: ${Math.round(this.maxX + this.padding)}px;`
+    }
+
+    private max(arr: any[], accessor: (a: any) => number) {
+        return Math.max.apply(null, arr.map(accessor))
+    }
+
+    private min(arr: any[], accessor: (a: any) => number) {
+        return Math.min.apply(null, arr.map(accessor))
+    }
+}
+
 export class Gear {
     public radius
     public teeth
     public teethHeight
     public teethAngle
+    public borders: Borders
 
     constructor() {
         this.teeth = 20
         this.teethAngle = Math.PI / 4
-        this.teethHeight = 20
+        this.teethHeight = 10
         this.radius = 0
     }
-
 
     public calc(): IPoint[] {
         // on calcule un polygone régulier dont le nombre de côtés est this.teeth et la longueur du côté est edgeLength
@@ -22,7 +55,6 @@ export class Gear {
         // rayon du cercle qui passe par tous les points de la base des dents (les Ai)
         // C'est aussi la longueur du côté du triangle décrit précédemment
         this.radius = edgeLength / Math.sin(innerAngle)
-        console.log('innerAngle', innerAngle, 'edgeLength', edgeLength, 'radius', this.radius)
         const points = []
         const firstPoint = { x: 0, y: this.radius }
         for (let i = 0; i < this.teeth; i++) {
@@ -30,18 +62,16 @@ export class Gear {
             points.push(rotatePoint(firstPoint, innerAngle * i))
         }
         points.push(points[0])
-        return points
+
+        return points.map(
+            p => translatePoint(p, { x: 4 * this.radius, y: 4 * this.radius })
+        )
     }
 
     public pointsToPath(points: IPoint[]): string {
-        // console.log(points)
-        // const first = points[0]
-        // let str = `M ${first.x},${first.y}`;
-
-        return points
-        // .map(p => ({ x: Math.round(p.x), y: Math.round(p.y) }))
-        .map(p => translatePoint(p, { x: 4 * this.radius, y: 4 * this.radius }))
-        .map(
+        this.borders = new Borders(points)
+        console.log(this.borders)
+        return points.map(
             (point, index) => {
                 return (index === 0)
                 ? `M ${point.x},${point.y}`
@@ -56,22 +86,15 @@ export class Gear {
         console.log('path', path)
         const ns = 'http://www.w3.org/2000/svg'
         const s = document.createElementNS(ns, 'svg')
-        // s.setAttribute('viewBox', `0 0 ${5 * this.radius} ${5 * this.radius}`)
-        s.setAttribute('viewBox', `0 0 600 600`)
+        s.setAttribute('viewBox', this.borders.asString())
+        s.setAttribute('style', this.borders.asStyle())
 
         const p = document.createElementNS(ns, 'path')
         p.setAttribute('fill', 'none')
         p.setAttribute('stroke', 'red')
         p.setAttribute('stroke-width', '1px')
         p.setAttribute('d', path)
-        // p.setAttribute('d', `M 70,135.3197264742181 L 13.431457505076196,102.65986323710905 L 13.431457505076189,37.34013676289097 L 69.99999999999999,4.680273525781914 L 126.56854249492379,37.34013676289093 L 126.56854249492383,102.659863237109 L 70,135.3197264742181`)
 
-
-        // p.setAttribute('d',
-        //     `M 10,10
-        //     L 90,90
-        //     V 10
-        //     H 50`)
         s.appendChild(p)
         return s
     }
